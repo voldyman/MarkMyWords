d180939139f985d2e08c5a99433ebfcbebe827e0
Akshay Shekher <voldyman666@gmail.com>
2014-12-24 17:13:27 +0530

    Added dependency, markdown lib

---
+++ deps/discount/src/discount-build/config.cmd
@@ -0,0 +1,2 @@
+#! /bin/sh
+  /home/voldyman/code/MarkMyWords/deps/discount/configure.sh  '--src=/home/voldyman/code/MarkMyWords/deps/discount/' '--prefix=/home/voldyman/code/MarkMyWords/deps/discount'
---
+++ deps/discount/src/discount-build/config.md
@@ -0,0 +1,5 @@
+#! /bin/sh
+# script generated Tue Dec 23 15:14:43 IST 2014 by configure.sh
+
+test -d "$1" || mkdir -p "$1"
+exit 0
