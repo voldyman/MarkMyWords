97ad133954fb4dc1b0ad011b66b20ff46972ac3c
Akshay Shekher <voldyman666@gmail.com>
2015-01-26 09:41:00 +0530

    removed discount build related files

--- deps/discount/config.cmd
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-  /home/voldyman/code/MarkMyWords/deps/discount/configure.sh  '--src=/home/voldyman/code/MarkMyWords/deps/discount' '--prefix=/home/voldyman/code/MarkMyWords/build' '--libdir=/home/voldyman/code/MarkMyWords/build'
--- deps/discount/config.md
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-# script generated Sun Jan 25 11:05:38 IST 2015 by configure.sh
-
-test -d "$1" || mkdir -p "$1"
-exit 0
